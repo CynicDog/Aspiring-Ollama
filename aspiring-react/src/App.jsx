50ad69a735c0b0f262064694a4db5c2030dcb525
ginsenglee <thecynicdog0328@gmail.com>
2024-07-30 19:38:35 +0900

    Chore: changed port of weather api server

--- aspiring-react/src/App.jsx
+++ aspiring-react/src/App.jsx
@@ -8,8 +8,7 @@ const App = () => {
             <header className="App-header">
                 <h2>React UI</h2>
             </header>
-            {/* temporally disalbed due to network settings (likely due to the port mapping of AspireReact_ApiService. Should be mapped to 8080 not the default value of 5329 in the `launchSettings.json`. */}
-            {/*<WeatherAPI />*/}
+            <WeatherAPI />
             <OllamaAPI />
         </div>
     );
