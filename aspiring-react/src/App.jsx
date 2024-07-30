f9a1f9aca11d6446e9107da5441afc7911edb2a9
이은상 <kyrsang0801@inbrein.com>
2024-07-30 16:55:30 +0900

    Chore: temporally disalbed `WeatherAPI` UI due to network settings (likely due to the port mapping of AspireReact_ApiService. Should be mapped to 8080 not the default value of 5329 in the `launchSettings.json`.

--- aspiring-react/src/App.jsx
+++ aspiring-react/src/App.jsx
@@ -8,7 +8,8 @@ const App = () => {
             <header className="App-header">
                 <h2>React UI</h2>
             </header>
-            <WeatherAPI />
+            {/* temporally disalbed due to network settings (likely due to the port mapping of AspireReact_ApiService. Should be mapped to 8080 not the default value of 5329 in the `launchSettings.json`. */}
+            {/*<WeatherAPI />*/}
             <OllamaAPI />
         </div>
     );
